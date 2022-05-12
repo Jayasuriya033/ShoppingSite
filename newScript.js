bf2c53eed9f8e4fd85e6c156f36d9fdd9bcd0ea0
Jayasuriya033 <95901720+Jayasuriya033@users.noreply.github.com>
2022-05-12 12:23:38 +0530

    Add files via upload

---
+++ newScript.js
@@ -0,0 +1,16 @@
+function big(img){
+    document.querySelector('.big').src=img;
+}
+function path(color){
+    const path=document.querySelector('.path');
+    path.style.background=color;
+}
+function bg(background){
+    const path=document.querySelector('.container');
+    bg.style.background=background;
+}
+
+// var a=10;
+// var b=20;
+// var c=a*b;
+// console.log("c is",c);
